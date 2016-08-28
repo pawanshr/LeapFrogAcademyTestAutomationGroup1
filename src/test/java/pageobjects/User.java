5e2fd8821100c313697824d2690ded6ead30c14c
Nischit Aryal <Nischit Aryal>
2016-08-28 09:30:52 +0545

    user test

--- src/test/java/pageobjects/User.java
+++ src/test/java/pageobjects/User.java
@@ -15,9 +15,6 @@ public  class User {
     @FindBy(xpath = ".//*[@id='secondary']/div/ul/div[1]/li[4]/ul/li[2]/a[1]/span")
     WebElement People;
 
-    @FindBy(xpath = ".//*[@id='secondary']/div/ul/div[1]/li[4]/ul/li[2]/a[1]/span")
-    WebElement People;
-
 
     public User(WebDriver driver) {
         this.driver = driver;
