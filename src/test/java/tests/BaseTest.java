b47788d07afc69a696953d0bad5fc3e4db4bf65a
katun182 <samankapali.kop@gmail.com>
2016-09-13 07:41:19 +0545

    Logout

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -29,7 +29,7 @@ public class BaseTest {
         Assert.assertEquals("Password",localloginPage.checkforPasswordLabel());
 
         loginPage.setUsername("admin");
-        loginPage.setPassword("admin");
+        loginPage.setPassword("qwerty123");
         loginPage.login();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
@@ -37,6 +37,6 @@ public class BaseTest {
 
     @After
     public void tearDown() {
-        driver.quit();
+        System.out.println("All Done Boss!");
     }
 }
